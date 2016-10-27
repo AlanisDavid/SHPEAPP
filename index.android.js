b52ee8f5a76f074cc64ae17ac787e994c9f77b22
Devany Sandoval <sandovad96@gmail.com>
2016-10-27 12:47:28 -0400

    test

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class SHPEAPP extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! Devany
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
