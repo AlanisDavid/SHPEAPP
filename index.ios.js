a98a0183445c220bb09deb44d424fafe627a082d
Devany Sandoval <sandovad96@gmail.com>
2016-12-29 20:47:37 -0500

    Button
    Basic set up for a button which will lead to next screen. Alert button
    for now.

--- index.ios.js
+++ index.ios.js
@@ -6,14 +6,8 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Image
-} from 'react-native';
-
+  AppRegistry, StyleSheet, Text, View, TextInput, Image, TouchableHighlight, Alert} from 'react-native';
+import MainScreen from './MainScreen';
 export default class SHPEAPP extends Component {
   render() {
     return (
@@ -22,22 +16,51 @@ export default class SHPEAPP extends Component {
         <Text style={styles.welcome}>
           Society of Hispanic Professional Engineers
         </Text>
-        <Text style={styles.instructions}>
-          Log in: 
-        </Text>
-         <TextInput
-                style={{height:40}}
-                placeholder="Username"
-                onChangeText={(text) => this.setState({text})}
-                />
-        <Text style={styles.instructions}>
-          Password:
-        </Text>
-        
+        <LogIn />
+        <LoginButton/>
       </View>
     );
   }
 }
+
+class LogIn extends Component {
+  render() {
+    return (
+      <View style= {{margin:10}} >
+      <View style = {styles.loginInput} >
+        <Text style={styles.directions}>
+          Log in:  </Text> 
+        <TextInput
+          style={styles.instructions}
+          placeholder= "Username"
+          onChangeText={(text) => this.setState({text})}/>
+        </View>
+        <View style = {styles.loginInput} >
+        <Text style={styles.directions}>
+          Password: </Text>
+        <TextInput
+          style={styles.instructions}
+          placeholder= "Password"
+          onChangeText={(text) => this.setState({text})}/>
+          </View>
+          </View>
+      );
+  }
+}
+
+class LoginButton extends Component {
+  _onPressButton() {
+    
+    Alert.alert("Button Pressed");
+  }
+  render() {
+    return (
+      <TouchableHighlight onPress={this._onPressButton}>
+      <Text>Button</Text>
+      </TouchableHighlight>
+    );
+  }
+} 
 
 const styles = StyleSheet.create({
   container: {
@@ -51,11 +74,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
+  directions: {
     color: '#333333',
     marginBottom: 5,
+    height: 18,
+    fontSize: 15, 
+    width:71
   },
+  instructions:{
+    color: '#333333',
+    marginBottom: 5,
+    height: 18,
+    fontSize: 15, 
+    width:101
+  },
+  loginInput : {
+    alignItems: 'center',
+    flexDirection: "row"
+  }
 });
 
 AppRegistry.registerComponent('SHPEAPP', () => SHPEAPP);
